181f651c4fbfe80bbd690b4821ad4650839707a6
wjywy <2934610933@qq.com>
2022-12-31 20:32:30 +0800

    '循环'

--- src/pages/assay/数据总览/bottomLeft/index.jsx
+++ src/pages/assay/数据总览/bottomLeft/index.jsx
@@ -3,25 +3,100 @@ import React,
     useRef,
     useEffect
 } from 'react';
-import * as echarts from 'echarts';
+import { useState } from 'react';
+import './index.css'
 
-function App() {
-    const mainEchart = useRef()
-    const initEcharts = () => {
-        let myEcharts
-        if (myEcharts != null && myEcharts !== "" && myEcharts !== undefined) {
-            myEcharts.dispose();//销毁
-        }
-        myEcharts = echarts.init(mainEchart.current,)   //初始化
-        let option = {
+const App = () => {
+    const bf_scroll = useRef(null)
+    useEffect(() => {
+        console.log(bf_scroll.current.scrollTop)
+        setInterval(() => {
+            bf_scroll.current.scrollTop = bf_scroll.current.scrollTop + 1
+            if (bf_scroll.current.scrollTop === 142) {
+                bf_scroll.current.scrollTop = 0
+            }
+        }, 100);
+    },[])
 
-        }
-    }
     return (
         <>
             <div>
-                <div>bottomLeft</div>
-                <div ref={mainEchart}></div>
+                <div style={{ textAlign: 'center', backgroundColor: '#56DFE2' }} className='index_bf_infor'>预警信息</div>
+                <div className='index_bottomLeft'>
+                    <div>用户</div>
+                    <div>患病概率</div>
+                    <div>相关指标</div>
+                </div>
+                <div className='index_element' ref={bf_scroll}>
+                    <div style={{height: 150}}></div>
+                    <div className='index_bottomLeft'>
+                        <div>重庆</div>
+                        <div>0.47</div>
+                        <div>兵临倒闭</div>
+                    </div>
+                    <div className='index_bottomLeft'>
+                        <div>重庆</div>
+                        <div>0.47</div>
+                        <div>兵临倒闭</div>
+                    </div>
+                    <div className='index_bottomLeft'>
+                        <div>重庆</div>
+                        <div>0.47</div>
+                        <div>兵临倒闭</div>
+                    </div>
+                    <div className='index_bottomLeft'>
+                        <div>重庆</div>
+                        <div>0.47</div>
+                        <div>兵临倒闭</div>
+                    </div>
+                    <div className='index_bottomLeft'>
+                        <div>重庆</div>
+                        <div>0.47</div>
+                        <div>兵临倒闭</div>
+                    </div>
+                    <div className='index_bottomLeft'>
+                        <div>重庆</div>
+                        <div>0.47</div>
+                        <div>兵临倒闭</div>
+                    </div>
+                    <div className='index_bottomLeft'>
+                        <div>重庆</div>
+                        <div>0.47</div>
+                        <div>兵临倒闭</div>
+                    </div>
+                    <div className='index_bottomLeft'>
+                        <div>重庆</div>
+                        <div>0.47</div>
+                        <div>兵临倒闭</div>
+                    </div>
+                    <div className='index_bottomLeft'>
+                        <div>重庆</div>
+                        <div>0.47</div>
+                        <div>兵临倒闭</div>
+                    </div>
+                    <div className='index_bottomLeft'>
+                        <div>重庆</div>
+                        <div>0.47</div>
+                        <div>兵临倒闭</div>
+                    </div>
+                    <div className='index_bottomLeft'>
+                        <div>重庆</div>
+                        <div>0.47</div>
+                        <div>兵临倒闭</div>
+                    </div>
+                    <div className='index_bottomLeft'>
+                        <div>重庆</div>
+                        <div>0.47</div>
+                        <div>兵临倒闭</div>
+                    </div>
+                    <div className='index_bottomLeft'>
+                        <div>重庆</div>
+                        <div>0.47</div>
+                        <div>兵临倒闭</div>
+                    </div>
+                </div>
+                {/* </div>
+                </div> */}
             </div>
         </>
     )
